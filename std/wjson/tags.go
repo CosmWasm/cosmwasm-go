21b665ad1ea18da9d6734d97b8a0d88bb017098e
MichaelWang <michael_wangguanyi@163.com>
2020-08-06 11:04:11 +0800

    FINISH: tinygo JSON marshal && unmarshal

---
+++ std/wjson/tags.go
@@ -0,0 +1,31 @@
+package wjson
+
+import "strings"
+
+type tagOptions string
+
+func parseTag(tag string) (string, tagOptions) {
+	if idx := strings.Index(tag, ","); idx != -1 {
+		return tag[:idx], tagOptions(tag[idx+1:])
+	}
+	return tag, tagOptions("")
+}
+
+func (o tagOptions) Contains(optionName string) bool {
+	if len(o) == 0 {
+		return false
+	}
+	s := string(o)
+	for s != "" {
+		var next string
+		i := strings.Index(s, ",")
+		if i >= 0 {
+			s, next = s[:i], s[i+1:]
+		}
+		if s == optionName {
+			return true
+		}
+		s = next
+	}
+	return false
+}
