ee2f2c1b48044e868666d88c8d81094ac585d65f
MichaelWang <michael_wangguanyi@163.com>
2020-08-13 10:03:30 +0800

    FINISH: safe arithmetic in math

--- std/safe_math/safe_math.go
+++ std/safe_math/safe_math.go
@@ -1,8 +1,37 @@
 package safe_math
 
-type SafeMath interface {
-	SafeAdd(a, b uint64) (uint64, error)
-	SafeSub(a, b uint64) (uint64, error)
-	SafeMul(a, b uint64) (uint64, error)
-	SafeDiv(a, b uint64) (uint64, error)
+import "errors"
+
+func SafeAdd(a, b uint64) (res uint64, err error) {
+	res = a + b
+	if res >= a && res >= b {
+		return
+	}
+	return res, errors.New("failed. overflow in add")
+}
+
+func SafeSub(a, b uint64) (res uint64, err error) {
+	if b > a {
+		return res, errors.New("failed. large subtractor")
+	}
+	return a - b, err
+}
+
+func SafeMul(a, b uint64) (res uint64, err error) {
+	res = a * b
+	if a == 0 || res/a == b {
+		return
+	}
+	return res, errors.New("failed. overflow in mul")
+}
+
+func SafeDiv(a, b uint64) (res uint64, err error) {
+	if b <= 0 {
+		return res, errors.New("failed. invalid divisor")
+	}
+	res = a / b
+	if res == b*res+a%b {
+		return
+	}
+	return res, errors.New("failed. overflow in div")
 }
