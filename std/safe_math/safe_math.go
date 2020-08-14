75f6a02caa8a5608f98df5c6b43e9ebe5f416756
MichaelWang <michael_wangguanyi@163.com>
2020-08-14 17:37:13 +0800

    OPTIMIZE: safe math

--- std/safe_math/safe_math.go
+++ std/safe_math/safe_math.go
@@ -1,18 +1,21 @@
 package safe_math
 
-import "errors"
+import (
+	"errors"
+	"strconv"
+)
 
 func SafeAdd(a, b uint64) (res uint64, err error) {
 	res = a + b
 	if res >= a && res >= b {
 		return
 	}
-	return res, errors.New("failed. overflow in add")
+	return res, errors.New("overflow in add")
 }
 
 func SafeSub(a, b uint64) (res uint64, err error) {
 	if b > a {
-		return res, errors.New("failed. large subtractor")
+		return res, errors.New("large subtractor with" + strconv.Itoa(int(b)))
 	}
 	return a - b, err
 }
@@ -22,16 +25,16 @@ func SafeMul(a, b uint64) (res uint64, err error) {
 	if a == 0 || res/a == b {
 		return
 	}
-	return 0, errors.New("failed. overflow in mul")
+	return 0, errors.New("overflow in mul")
 }
 
 func SafeDiv(a, b uint64) (res uint64, err error) {
-	if b <= 0 {
-		return res, errors.New("failed. invalid divisor")
+	if b == 0 {
+		return res, errors.New("invalid divisor with 0")
 	}
 	res = a / b
 	if a == b*res+a%b {
 		return
 	}
-	return res, errors.New("failed. overflow in div")
+	return res, errors.New("overflow in div")
 }
