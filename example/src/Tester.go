12d5da8d1e15e01dd1ec3eb4f7b3accd12b1fbbd
KamiD <yzglwt@sina.com>
2020-09-04 13:59:06 +0800

    commit api test

--- example/src/Tester.go
+++ example/src/Tester.go
@@ -15,6 +15,10 @@ func (t Tester) DoTest() error {
 	if e != nil {
 		return errors.New("Db test failed : " + e.Error())
 	}
+	e = t.apiTest()
+	if e != nil {
+		return errors.New("API test failed : " + e.Error())
+	}
 	return nil
 }
 
@@ -34,10 +38,6 @@ func (t Tester) dbTest() error {
 		return e
 	}
 	e = t.doRemoveDbTest()
-	if e != nil {
-		return e
-	}
-	e = t.doRangeDbTest()
 	if e != nil {
 		return e
 	}
@@ -87,7 +87,39 @@ func (t Tester) doRemoveDbTest() error {
 	return nil
 }
 
+//not support for now
 func (t Tester) doRangeDbTest() error {
+	testKv := t.data()
 	t.doWriteDbTest() //make sure we have right test data in db
+
+	iter, e := t.deps.EStorage.Range(testKv[0], testKv[len(testKv)-1], std.Ascending)
+	if e != nil {
+		return errors.New("Range failed by init : " + e.Error())
+	}
+	for {
+		k, v, e := iter.Next()
+		if e != nil {
+			break
+		}
+		std.DisplayMessage([]byte("Reading key " + string(k) + ",value " + string(v)))
+	}
+	return nil
+}
+
+func (t Tester) apiTest() error {
+	humanAddress := "ABCDEFG1234567890"
+	canonicalAddr, e := t.deps.EApi.CanonicalAddress(humanAddress)
+	if e != nil {
+		return errors.New("CanonicalAddress failed, error " + e.Error())
+	}
+
+	origAddr, e := t.deps.EApi.HumanAddress(canonicalAddr)
+	if e != nil {
+		return errors.New("HumanAddress failed, error " + e.Error())
+	}
+	if origAddr != humanAddress {
+		return errors.New("Translate error, excepted " + humanAddress + ", found " + origAddr)
+	}
+	std.DisplayMessage([]byte("Translate succeed, excepted " + humanAddress + ", found " + origAddr))
 	return nil
 }
