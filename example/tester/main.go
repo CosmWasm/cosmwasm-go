05fb203934a5bd2f834e73e8f5e60f75f559442f
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-21 17:28:40 +0200

    tester: Pulled the exports into main.go, not in src/

--- example/tester/main.go
+++ example/tester/main.go
@@ -1,5 +1,26 @@
 package main
 
-import _ "github.com/cosmwasm/cosmwasm-go/example/tester/src"
+import "C"
+
+import (
+	"github.com/cosmwasm/cosmwasm-go/std"
+	"github.com/cosmwasm/cosmwasm-go/example/tester/src"
+	"unsafe"
+)
 
 func main() {}
+
+//export init
+func initialize(env_ptr uint32, msg_ptr uint32) unsafe.Pointer {
+	return std.DoInit(src.Init, env_ptr, msg_ptr)
+}
+
+//export handle
+func handle(env_ptr uint32, msg_ptr uint32) unsafe.Pointer {
+	return std.DoHandler(src.Invoke, env_ptr, msg_ptr)
+}
+
+//export query
+func query(msg_ptr uint32) unsafe.Pointer {
+	return std.DoQuery(src.Query, msg_ptr)
+}
