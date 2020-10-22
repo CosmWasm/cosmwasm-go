b7ba8d3d3e9d51602e0cf8a8ed9e0b314b25c7fb
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-22 22:21:24 +0200

    Update env struct types and write first erc20 tests

--- example/erc20/src/contract_test.go
+++ example/erc20/src/contract_test.go
@@ -1,1 +1,34 @@
 package src
+
+import (
+	"testing"
+
+	"github.com/cosmwasm/cosmwasm-go/std"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInit(t *testing.T) {
+	cases := map[string]struct {
+		initMsg []byte
+		valid   bool
+	}{
+		"invalid json": {initMsg: []byte("{...")},
+		"wrong struct": {initMsg: []byte(`{"foo": 1, "bar": "world"}`)},
+		"proper init":  {initMsg: []byte(`{"name":"Cool Coin","symbol":"COOL","decimal":6,"total_supply":12345678}`)},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			deps := std.MockExtern()
+			env := std.MockEnv("creator", nil)
+			res, err := Init(deps, env, tc.initMsg)
+			if tc.valid {
+				require.Nil(t, err)
+				require.NotNil(t, res)
+			} else {
+				require.NotNil(t, err)
+				require.Nil(t, res)
+			}
+		})
+	}
+}
