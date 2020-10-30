dab372915a63c89c31ba7f231700ddda8ee4bf14
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-30 01:41:33 +0100

    De-duplicate lots of integration test code

--- example/erc20/integration/integration_test.go
+++ example/erc20/integration/integration_test.go
@@ -2,60 +2,40 @@ package integration
 
 import (
 	"encoding/json"
-	"io/ioutil"
-	"os"
 	"path/filepath"
 	"testing"
 
-	cosmwasm "github.com/CosmWasm/go-cosmwasm"
+	cosmwasm "github.com/CosmWasm/go-cosmwasm/api"
 	types "github.com/CosmWasm/go-cosmwasm/types"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cosmwasm/cosmwasm-go/example/erc20/src"
 	"github.com/cosmwasm/cosmwasm-go/std"
 	"github.com/cosmwasm/cosmwasm-go/std/ezjson"
+	"github.com/cosmwasm/cosmwasm-go/std/integration"
 )
 
 var CONTRACT = filepath.Join("..", "erc20.wasm")
 
-const FEATURES = "staking"
-const mockContractAddr = "coral1lstq3dy9v0s86czkx0rvgwnmunds5y2lz53all"
-
-func loadCode(t *testing.T) []byte {
-	bz, err := ioutil.ReadFile(CONTRACT)
-	require.NoError(t, err)
-	return bz
-}
-
 func TestWorkflow(t *testing.T) {
-	// setup wasmer instance
-	tmpdir, err := ioutil.TempDir("", "erc20")
-	require.NoError(t, err)
-	defer os.RemoveAll(tmpdir)
-	wasmer, err := cosmwasm.NewWasmer(tmpdir, FEATURES, true)
-	require.NoError(t, err)
-
-	// upload code and get some sha256 hash
-	codeID, err := wasmer.Create(loadCode(t))
-	require.NoError(t, err)
-	require.Equal(t, 32, len(codeID))
+	wasmer, codeID := integration.SetupWasmer(t, CONTRACT)
 
 	// a whole lot of setup object using go-cosmwasm mock/test code
 	var gasLimit uint64 = 100_000_000
-	gasMeter := NewMockGasMeter(gasLimit)
-	store := NewLookup(gasMeter)
-	api := NewMockAPI()
-	querier := DefaultQuerier(mockContractAddr, types.Coins{types.NewCoin(100, "ATOM")})
-	info := mockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", nil)
+	gasMeter := cosmwasm.NewMockGasMeter(gasLimit)
+	store := cosmwasm.NewLookup(gasMeter)
+	api := cosmwasm.NewMockAPI()
+	querier := cosmwasm.DefaultQuerier(cosmwasm.MOCK_CONTRACT_ADDR, types.Coins{types.NewCoin(100, "ATOM")})
+	info := cosmwasm.MockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", nil)
 
 	initMsg := []byte(`{"name":"OKB","symbol":"OKB","decimal":10,"total_supply":170000}`)
 	//initMsg := []byte(`{123]]`) // invalid json
 	res, _, err := wasmer.Instantiate(codeID,
-		mockEnv(),
+		cosmwasm.MockEnv(),
 		info,
 		initMsg,
 		store,
-		api,
+		*api,
 		querier,
 		gasMeter,
 		gasLimit,
@@ -71,11 +51,11 @@ func TestWorkflow(t *testing.T) {
 
 	handleMsg := []byte(`{"Transfer":{"to":"1234567","value": 2000}}`)
 	_, _, err = wasmer.Execute(codeID,
-		mockEnv(),
+		cosmwasm.MockEnv(),
 		info,
 		handleMsg,
 		store,
-		api,
+		*api,
 		querier,
 		gasMeter,
 		gasLimit,
@@ -85,10 +65,10 @@ func TestWorkflow(t *testing.T) {
 
 	queryMsg := []byte(`{"balance":{"address":"1234567"}}`)
 	qres, _, err := wasmer.Query(codeID,
-		mockEnv(),
+		cosmwasm.MockEnv(),
 		queryMsg,
 		store,
-		api,
+		*api,
 		querier,
 		gasMeter,
 		gasLimit,
@@ -114,7 +94,7 @@ func TestInfoMarshalCompatibility(t *testing.T) {
 
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
-			info := mockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", tc.funds)
+			info := cosmwasm.MockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", tc.funds)
 			bz, err := json.Marshal(info)
 			require.NoError(t, err)
 
@@ -131,34 +111,24 @@ func TestInfoMarshalCompatibility(t *testing.T) {
 }
 
 func TestErrorReturned(t *testing.T) {
-	// setup wasmer instance
-	tmpdir, err := ioutil.TempDir("", "erc20")
-	require.NoError(t, err)
-	defer os.RemoveAll(tmpdir)
-	wasmer, err := cosmwasm.NewWasmer(tmpdir, FEATURES, true)
-	require.NoError(t, err)
-
-	// upload code and get some sha256 hash
-	codeID, err := wasmer.Create(loadCode(t))
-	require.NoError(t, err)
-	require.Equal(t, 32, len(codeID))
+	wasmer, codeID := integration.SetupWasmer(t, CONTRACT)
 
 	// a whole lot of setup object using go-cosmwasm mock/test code
 	var gasLimit uint64 = 100_000_000
-	gasMeter := NewMockGasMeter(gasLimit)
-	store := NewLookup(gasMeter)
-	api := NewMockAPI()
-	querier := DefaultQuerier(mockContractAddr, nil)
-	info := mockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", nil)
+	gasMeter := cosmwasm.NewMockGasMeter(gasLimit)
+	store := cosmwasm.NewLookup(gasMeter)
+	api := cosmwasm.NewMockAPI()
+	querier := cosmwasm.DefaultQuerier(cosmwasm.MOCK_CONTRACT_ADDR, nil)
+	info := cosmwasm.MockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", nil)
 
 	// this doesn't create json parse error, but we error on validating an empty InitMsg
 	initMsg := []byte(`{.32r!`)
-	_, _, err = wasmer.Instantiate(codeID,
-		mockEnv(),
+	_, _, err := wasmer.Instantiate(codeID,
+		cosmwasm.MockEnv(),
 		info,
 		initMsg,
 		store,
-		api,
+		*api,
 		querier,
 		gasMeter,
 		gasLimit,
@@ -169,59 +139,29 @@ func TestErrorReturned(t *testing.T) {
 }
 
 func TestWorkflowWithFunds(t *testing.T) {
-	// setup wasmer instance
-	tmpdir, err := ioutil.TempDir("", "erc20")
-	require.NoError(t, err)
-	defer os.RemoveAll(tmpdir)
-	wasmer, err := cosmwasm.NewWasmer(tmpdir, FEATURES, true)
-	require.NoError(t, err)
-
-	// upload code and get some sha256 hash
-	codeID, err := wasmer.Create(loadCode(t))
-	require.NoError(t, err)
-	require.Equal(t, 32, len(codeID))
+	wasmer, codeID := integration.SetupWasmer(t, CONTRACT)
 
 	// a whole lot of setup object using go-cosmwasm mock/test code
 	var gasLimit uint64 = 100_000_000
-	gasMeter := NewMockGasMeter(gasLimit)
-	store := NewLookup(gasMeter)
-	api := NewMockAPI()
+	gasMeter := cosmwasm.NewMockGasMeter(gasLimit)
+	store := cosmwasm.NewLookup(gasMeter)
+	api := cosmwasm.NewMockAPI()
 	funds := types.Coins{types.NewCoin(1000, "uatom"), types.NewCoin(60000, "utgd")}
-	querier := DefaultQuerier(mockContractAddr, funds)
-	info := mockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", funds)
+	querier := cosmwasm.DefaultQuerier(cosmwasm.MOCK_CONTRACT_ADDR, funds)
+	info := cosmwasm.MockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", funds)
 
 	initMsg := []byte(`{"name":"Okay","symbol":"OKB","decimal":10,"total_supply":170000}`)
 	res, gas, err := wasmer.Instantiate(codeID,
-		mockEnv(),
+		cosmwasm.MockEnv(),
 		info,
 		initMsg,
 		store,
-		api,
+		*api,
 		querier,
 		gasMeter,
 		gasLimit,
 	)
 	require.NoError(t, err)
 	require.NotNil(t, res)
-	require.Equal(t, uint64(0xe4cb6), gas)
-}
-
-func mockEnv() types.Env {
-	return types.Env{
-		Block: types.BlockInfo{
-			Height:  123,
-			Time:    1578939743,
-			ChainID: "foobar",
-		},
-		Contract: types.ContractInfo{
-			Address: mockContractAddr,
-		},
-	}
-}
-
-func mockInfo(sender types.HumanAddress, sentFunds []types.Coin) types.MessageInfo {
-	return types.MessageInfo{
-		Sender:    sender,
-		SentFunds: sentFunds,
-	}
+	require.Equal(t, uint64(0xe3e23), gas)
 }
--- example/hackatom/integration/integration_test.go
+++ example/hackatom/integration/integration_test.go
@@ -2,60 +2,36 @@ package integration
 
 import (
 	"encoding/json"
-	"io/ioutil"
-	"os"
 	"path/filepath"
 	"testing"
 
-	cosmwasm "github.com/CosmWasm/go-cosmwasm"
-	types "github.com/CosmWasm/go-cosmwasm/types"
+	cosmwasm "github.com/CosmWasm/go-cosmwasm/api"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cosmwasm/cosmwasm-go/example/hackatom/src"
+	"github.com/cosmwasm/cosmwasm-go/std/integration"
 )
 
 var CONTRACT = filepath.Join("..", "hackatom.wasm")
 
-const FEATURES = "staking"
-const mockContractAddr = "coral1lstq3dy9v0s86czkx0rvgwnmunds5y2lz53all"
-
-func setup(t *testing.T) (*cosmwasm.Wasmer, []byte) {
-	// setup wasmer instance
-	tmpdir, err := ioutil.TempDir("", "erc20")
-	require.NoError(t, err)
-	t.Cleanup(func() { os.RemoveAll(tmpdir) })
-
-	wasmer, err := cosmwasm.NewWasmer(tmpdir, FEATURES, true)
-	require.NoError(t, err)
-
-	// upload code and get some sha256 hash
-	bz, err := ioutil.ReadFile(CONTRACT)
-	require.NoError(t, err)
-	codeID, err := wasmer.Create(bz)
-	require.NoError(t, err)
-	require.Equal(t, 32, len(codeID))
-
-	return wasmer, codeID
-}
-
 func TestWorkflow(t *testing.T) {
-	wasmer, codeID := setup(t)
+	wasmer, codeID := integration.SetupWasmer(t, CONTRACT)
 
 	// a whole lot of setup object using go-cosmwasm mock/test code
 	var gasLimit uint64 = 100_000_000
-	gasMeter := NewMockGasMeter(gasLimit)
-	store := NewLookup(gasMeter)
-	api := NewMockAPI()
-	querier := DefaultQuerier(mockContractAddr, nil)
+	gasMeter := cosmwasm.NewMockGasMeter(gasLimit)
+	store := cosmwasm.NewLookup(gasMeter)
+	api := cosmwasm.NewMockAPI()
+	querier := cosmwasm.DefaultQuerier(cosmwasm.MOCK_CONTRACT_ADDR, nil)
 
-	info := mockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", nil)
+	info := cosmwasm.MockInfo("coral1e86v774dch5uwkks0cepw8mdz8a9flhhapvf6w", nil)
 	initMsg := []byte(`{"count":1234}`)
 	res, _, err := wasmer.Instantiate(codeID,
-		mockEnv(),
+		cosmwasm.MockEnv(),
 		info,
 		initMsg,
 		store,
-		api,
+		*api,
 		querier,
 		gasMeter,
 		gasLimit,
@@ -65,10 +41,10 @@ func TestWorkflow(t *testing.T) {
 
 	queryMsg := []byte(`{"get_count":{"a":"b"}}`)
 	qres, _, err := wasmer.Query(codeID,
-		mockEnv(),
+		cosmwasm.MockEnv(),
 		queryMsg,
 		store,
-		api,
+		*api,
 		querier,
 		gasMeter,
 		gasLimit,
@@ -82,24 +58,4 @@ func TestWorkflow(t *testing.T) {
 	err = json.Unmarshal(qres, &count)
 	require.NoError(t, err)
 	require.Equal(t, uint64(1234), count.Count)
-}
-
-func mockEnv() types.Env {
-	return types.Env{
-		Block: types.BlockInfo{
-			Height:  123,
-			Time:    1578939743,
-			ChainID: "foobar",
-		},
-		Contract: types.ContractInfo{
-			Address: mockContractAddr,
-		},
-	}
-}
-
-func mockInfo(sender types.HumanAddress, sentFunds []types.Coin) types.MessageInfo {
-	return types.MessageInfo{
-		Sender:    sender,
-		SentFunds: sentFunds,
-	}
 }
