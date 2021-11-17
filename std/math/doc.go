631438f469e099470c3166a39bf2850d1587c376
fdymylja <frojdi.dymylja@gmail.com>
2021-11-17 13:42:41 +0200

    add: uint128 implementation

---
+++ std/math/doc.go
@@ -0,0 +1,4 @@
+// Package math contains types that can be used to work with
+// predefined precision decimals and unsigned integers from
+// smart contract in a safe, deterministic and user-friendly way.
+package math
