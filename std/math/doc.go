ea757306e50d73a2d1a4056325e69044de63535c
Frojdi Dymylja <33157909+fdymylja@users.noreply.github.com>
2021-11-19 12:49:37 +0200

    add: uint128 implementation (#91)
    * add: uint128 implementation
    
    * chore: update doc
    
    * add: safe div/div64 api
    
    * fix: remove usage of fmt
    
    * change: distinguish little endian from big endian byte order
    
    * change: remove big.Int dependency
    
    * add: unoptimized uint128 string parser and acceptance tests
    
    * chore: refer original source cod in docs.
    
    * chore: cleanup acceptance tests
    
    * fix: add safety in fromstring
    
    * chore: add more acceptance tests

---
+++ std/math/doc.go
@@ -0,0 +1,5 @@
+// Package math contains types that can be used to work with
+// predefined precision decimals and unsigned integers from
+// smart contract in a safe, deterministic and user-friendly way.
+// Based on https://github.com/lukechampine/uint128, LICENSE: https://github.com/lukechampine/uint128/blob/master/LICENSE.
+package math
