1567ca864f64a5f1106b1edd9bc6c0e35b4ef4c5
fdymylja <frojdi.dymylja@gmail.com>
2021-12-03 15:16:36 +0100

    fix: namespaced.Range and add tests

--- std/storage/namespaced.go
+++ std/storage/namespaced.go
@@ -1,6 +1,8 @@
 package storage
 
-import "github.com/cosmwasm/cosmwasm-go/std"
+import (
+	"github.com/cosmwasm/cosmwasm-go/std"
+)
 
 const (
 	// NamespaceTerminator is the uint8 that terminates a namespace.
@@ -37,7 +39,7 @@ func (s Namespaced) Remove(storage std.Storage, key []byte) {
 // Range ranges over the given start to end range after prepending the namespace prefix.
 func (s Namespaced) Range(storage std.Storage, start, end []byte, order std.Order) std.Iterator {
 	nsStart := s.key(start)
-	nsEnd := s.key(end)
+	nsEnd := s.rangeEndKey(end)
 	iter := storage.Range(nsStart, nsEnd, order)
 
 	return NamespacedIterator{prefixLength: len(s.prefix), iter: iter}
@@ -46,6 +48,30 @@ func (s Namespaced) Range(storage std.Storage, start, end []byte, order std.Orde
 // key returns the key with the namespace prefix.
 func (s Namespaced) key(k []byte) []byte {
 	return append(s.prefix, k...)
+}
+
+// rangeEndKey does special handling for a nil ending key
+// because we need to basically add one byte to the prefix.
+func (s Namespaced) rangeEndKey(end []byte) []byte {
+	if end != nil {
+		return s.key(end)
+	}
+
+	e := make([]byte, len(s.prefix))
+	copy(e, s.prefix)
+
+	for i := len(s.prefix) - 1; i >= 0; i-- {
+		if e[i] < byte(0xFF) {
+			e[i]++
+			return e
+		}
+		e[i] = byte(0x00)
+		if i == 0 {
+			return nil
+		}
+	}
+
+	return nil
 }
 
 // NamespacedIterator is the std.Iterator of namespaced storage.
--- std/storage/namespaced_test.go
+++ std/storage/namespaced_test.go
@@ -1,3 +1,39 @@
 package storage
 
+import (
+	"github.com/cosmwasm/cosmwasm-go/std"
+	"github.com/cosmwasm/cosmwasm-go/std/mocks"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
 // TODO(fdymylja): test this
+
+func TestNamespaced(t *testing.T) {
+	storage := mocks.NewMockStorage()
+	ns := NewNamespaced("prefix")
+
+	ns.Set(storage, []byte("1"), []byte("a"))
+	ns.Set(storage, []byte("11"), []byte("aa"))
+
+	iter := ns.Range(storage, []byte("1"), nil, std.Ascending)
+	// test iterator strips prefix bytes
+	k, v, err := iter.Next()
+	require.NoError(t, err)
+	require.Equal(t, []byte("1"), k)
+	require.Equal(t, []byte("a"), v)
+
+	k, v, err = iter.Next()
+	require.NoError(t, err)
+	require.Equal(t, []byte("11"), k)
+	require.Equal(t, []byte("aa"), v)
+
+	// test get
+	v = ns.Get(storage, []byte("1"))
+	require.Equal(t, []byte("a"), v)
+
+	// test remove
+	ns.Remove(storage, []byte("1"))
+	require.Nil(t, storage.Get([]byte("1")))
+
+}
