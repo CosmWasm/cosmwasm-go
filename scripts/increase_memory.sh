c213d4dc067c38584d1cf1defa8f98f1ede013c3
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-12-15 13:49:02 +0100

    Expand memory (#120)
    * Increase page size by modifying the wasm file
    
    * Compile hackatom.wasm with 20 pages
    
    * Try 2000 depth recursion, that triggered bug before

---
+++ scripts/increase_memory.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+EMSCRIPTEN="polkasource/webassembly-wabt:v1.0.11"
+
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+ROOT="$(dirname "$SCRIPT_DIR")"
+
+function print_usage() {
+  echo "Usage: $0 [wasm_file] (memory_pages)"
+  echo ""
+  echo "e.g. $0 hackatom.wasm 12"
+  echo "This file must have been compiled by compile.sh before and located in $ROOT"
+  echo "If no memory pages is set, defaults to 20"
+}
+
+if [ "$#" -ne 1 ]; then
+    print_usage
+    exit 1
+fi
+
+FILE="${1}"
+
+if [ ! -f "$FILE" ]; then
+    print_usage
+    exit 1
+fi
+
+PAGES="${2:-20}"
+
+WATFILE=$(echo "${FILE}" | sed 's/\.wasm/\.wat/')
+
+echo "Increasing memory pages of $FILE to $PAGES"
+
+docker run --rm --platform linux/amd64 -v "${ROOT}:/code" ${EMSCRIPTEN} wasm2wat "/code/${FILE}" > "${WATFILE}"
+sed -i "s/(memory (;0;) 2)/(memory (;0;) $PAGES)/" "${WATFILE}"
+docker run --rm --platform linux/amd64 -v "${ROOT}:/code" ${EMSCRIPTEN} wat2wasm "/code/${WATFILE}" -o "/code/$FILE"
+
+echo ""
+ls -l "${FILE}"
+sha256sum "${FILE}"
+
