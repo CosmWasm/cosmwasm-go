d857bdce579f1cdc76ad8d4ac7ccc2e9bf859629
fdymylja <frojdi.dymylja@gmail.com>
2021-12-26 14:49:25 +0100

    add: tests

--- example/identityv2/src/contract_test.go
+++ example/identityv2/src/contract_test.go
@@ -1,14 +1,109 @@
 package src
 
 import (
-	"github.com/cosmwasm/cosmwasm-go/pkg/generator"
+	"github.com/cosmwasm/cosmwasm-go/std/mock"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-func TestGenerateContract(t *testing.T) {
-	gen, err := generator.NewContract("src", Contract{})
+func TestContract_ExecCreateIdentity(t *testing.T) {
+	deps := mock.Deps(nil)
+	env := mock.Env()
+	info := mock.Info("sender", nil)
+
+	// test create
+	_, err := Contract{}.ExecCreateIdentity(deps, &env, &info, &MsgCreateIdentity{
+		Name:       "name",
+		Surname:    "surname",
+		City:       "city",
+		PostalCode: 100,
+	})
 	require.NoError(t, err)
-	require.NoError(t, gen.Generate())
-	require.NoError(t, gen.WriteTo("contract.boilerplate.go"))
+
+	// check if it exists
+	person, err := Contract{}.QueryIdentity(deps, &env, &QueryIdentity{
+		ID: info.Sender,
+	})
+	require.NoError(t, err)
+	require.Equal(t, Person{
+		Address:    info.Sender,
+		Name:       "name",
+		Surname:    "surname",
+		City:       "city",
+		PostalCode: 100,
+	}, *person)
+
+	// check can create only once
+	_, err = Contract{}.ExecCreateIdentity(deps, &env, &info, &MsgCreateIdentity{
+		Name:       "name",
+		Surname:    "surname",
+		City:       "city",
+		PostalCode: 100,
+	})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), ErrPersonAlreadyExists.Error())
+}
+
+func TestContract_ExecDeleteIdentity(t *testing.T) {
+	deps := mock.Deps(nil)
+	env := mock.Env()
+	info := mock.Info("sender", nil)
+
+	// create
+	_, err := Contract{}.ExecCreateIdentity(deps, &env, &info, &MsgCreateIdentity{
+		Name:       "name",
+		Surname:    "surname",
+		City:       "city",
+		PostalCode: 100,
+	})
+	require.NoError(t, err)
+	// delete
+	_, err = Contract{}.ExecDeleteIdentity(deps, &env, &info, &MsgDelete{})
+	require.NoError(t, err)
+
+	// test can't delete
+	_, err = Contract{}.QueryIdentity(deps, &env, &QueryIdentity{ID: info.Sender})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), ErrPersonNotFound.Error())
+}
+
+func TestContract_ExecUpdateCity(t *testing.T) {
+	deps := mock.Deps(nil)
+	env := mock.Env()
+	info := mock.Info("sender", nil)
+
+	// test error if person does not exist
+	_, err := Contract{}.ExecUpdateCity(deps, &env, &info, &MsgUpdateCity{
+		City:       "othercity",
+		PostalCode: 300,
+	})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), ErrPersonNotFound.Error())
+
+	// create
+	_, err = Contract{}.ExecCreateIdentity(deps, &env, &info, &MsgCreateIdentity{
+		Name:       "name",
+		Surname:    "surname",
+		City:       "city",
+		PostalCode: 100,
+	})
+	require.NoError(t, err)
+
+	// update
+	_, err = Contract{}.ExecUpdateCity(deps, &env, &info, &MsgUpdateCity{
+		City:       "othercity",
+		PostalCode: 1000,
+	})
+	require.NoError(t, err)
+
+	person, err := Contract{}.QueryIdentity(deps, &env, &QueryIdentity{ID: info.Sender})
+	require.NoError(t, err)
+
+	require.Equal(t, *person, Person{
+		Address:    info.Sender,
+		Name:       "name",
+		Surname:    "surname",
+		City:       "othercity",
+		PostalCode: 1000,
+	})
 }
